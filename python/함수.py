cbd6214fac483371eed9e4d226c837a08f47b62a
ruby <dhdmsdud97@gmail.com>
2021-01-09 22:46:04 +0900

    update

--- python/함수.py
+++ python/함수.py
@@ -40,3 +40,58 @@ profile("kim")
 profile("yoo")
 
 # 가변인자
+def profile(name, age, *language):
+    print("name:{} age:{}".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+
+
+profile("yoo", 20, "python", "java", "c++", "c#", "javascript")
+print("kim", 25, "kotlin", "swift")
+
+# 지역변수(함수가 끝나면 사라짐), 전역변수(모든공간에서 사용)
+gun = 10
+
+def checkpoint(soldiers):  # 총을 들고 경계근무나가는 군인
+    global gun
+    gun = gun - soldiers
+    print("남은 총 :{}".format(gun))
+
+print("전체 총:{}".format(gun))
+checkpoint(2) # 2명이 경계근무
+print("남은 총:{}".format(gun))
+
+gun = 20
+def checkpoint_return(gun, soldiers) :
+    gun = gun - soldiers
+    print("남은 총:{}".format(gun))
+    return gun
+
+gun = checkpoint_return(gun, 14)
+print("남은 총:{}".format(gun))
+
+# 표준체중 구하기
+# 남자 : 키 * 키 * 22
+# 여자 : 키 * 키 * 21
+# 조건 1) 함수명 : std_weight , 전달값 : 키, 성별
+
+def std_wight(gender, height):
+    if gender == "여자" :
+        print("키{}cm {}의 표준체중은 {}입니다.".format(height, gender, round(height/100 * height/100 *21), 2))
+    else:
+        print("키{}cm {}의 표준체중은 {}입니다.".format(height, gender, height/100 * height/100 *22))
+
+std_wight("여자", 155)
+
+
+
+
+
+
+
+
+
+
+
+
+
