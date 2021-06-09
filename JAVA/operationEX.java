97edf98011464e2a2335e2cf4156c08924cabc05
ruby <dhdmsdud97@gmail.com>
2021-06-09 20:19:19 +0900

    JAVA

---
+++ JAVA/operationEX.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class operationEX {
+
+	public static void main(String[] args) {
+		int age = 24;
+		System.out.println(age);
+		
+		int num = 10;
+		int num2 = -num;
+		System.out.println(num);
+		System.out.println(num2);
+		
+		num = -num;
+		System.out.println(num);
+	}
+
+}
