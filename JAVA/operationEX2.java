add81209e16d9dbdfffa3e5b60f88ca2d7f23068
ruby <dhdmsdud97@gmail.com>
2021-06-10 20:19:19 +0900

    JAVA

---
+++ JAVA/operationEX2.java
@@ -0,0 +1,23 @@
+package assignment;
+
+public class operationEX2 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(num++);
+		System.out.println(num);
+
+		int num1 = 10;
+		System.out.println(num1--);
+		System.out.println(num1);
+		
+		System.out.println(3<5);
+
+		int num3 = 3;
+		int num4 = 5;
+		boolean flag = (num3 > num4);
+		System.out.println(flag);
+	
+	}
+
+}
