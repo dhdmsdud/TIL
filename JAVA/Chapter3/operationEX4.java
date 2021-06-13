813805f5eab35035264112d06badc8dfdc96c3db
ruby <dhdmsdud97@gmail.com>
2021-06-13 20:19:19 +0900

    JAVA

---
+++ JAVA/Chapter3/operationEX4.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class operationEX4 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		System.out.println(num1 += 1);
+		System.out.println(num1 %= 1);
+		System.out.println(num1 -= 1);
+		
+		int num = (5>3) ? 10 : 20;
+		System.out.println(num);
+		
+		int num2 = (5<3) ? 10 : 20;
+		System.out.println(num2);
+	}
+
+}
