d9ad829a7f82776a1a6594688d336ed22cfc334e
ruby <dhdmsdud97@gmail.com>
2021-06-17 23:58:55 +0900

    JAVA

---
+++ JAVA/ImplicitConversion.java
@@ -0,0 +1,18 @@
+package binary;
+
+public class ImplictConversion {
+
+	public static void main(String[] args) {
+		byte bNum = 10;
+		int num = bNum;
+		System.out.println(num);
+		
+		long lNum = 10;
+		float fNum = lNum;
+		System.out.println(fNum);
+		
+		double dNum = fNum = num;
+		System.out.println(dNum);
+	}
+
+}
