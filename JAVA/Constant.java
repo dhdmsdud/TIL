08cc730bc46d601441808d01ba785fa73dcaa356
ruby <dhdmsdud97@gmail.com>
2021-06-18 23:48:05 +0900

    JAVA

---
+++ JAVA/Constant.java
@@ -0,0 +1,18 @@
+package binary;
+
+public class Constant {
+
+	public static void main(String[] args) {
+
+		final int max_num = 100;
+		final float pi = 3.14f;
+		
+		final int Student_num = 30;
+		
+		int num = 0;
+		if (num == Student_num) {}
+		
+		System.out.println(Student_num);
+		
+	}	
+}
