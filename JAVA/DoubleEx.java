103bbe4a58bb18cfd2081cc1dd5733f0c21d0c9d
ruby <dhdmsdud97@gmail.com>
2021-06-20 09:40:10 +0900

    JAVA

---
+++ JAVA/DoubleEx.java
@@ -0,0 +1,15 @@
+package binary;
+
+public class DoubleEx {
+
+	public static void main(String[] args) {
+		double dNum = 3.14;
+		var eNum = 3.14;
+		float fNum = 3.14F;
+		System.out.println(dNum);
+		System.out.println(eNum);
+		System.out.println(fNum);
+
+	}
+
+}
