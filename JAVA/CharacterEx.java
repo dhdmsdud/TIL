acb26d6d5a8d9f00a8abf9e09b0b0e7ebb677ba2
ruby <dhdmsdud97@gmail.com>
2021-06-21 17:40:10 +0900

    JAVA

---
+++ JAVA/CharacterEx.java
@@ -0,0 +1,19 @@
+package binary;
+
+public class CharacterEx {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		System.out.println(ch);
+		System.out.println((int)ch);
+		
+		ch = 66;
+		System.out.println(ch);
+		
+		int ch2 = 67;
+		System.out.println(ch2);
+		System.out.println((char)ch2);
+		
+	}
+
+}
