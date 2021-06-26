65fe7f153bfe3a9b4cb4eb4dbdb31ed5c9846d41
ruby <dhdmsdud97@gmail.com>
2021-06-26 12:55:53 +0900

    JAVA

---
+++ JAVA/BinaryTest2.java
@@ -0,0 +1,18 @@
+package binary;
+
+public class BinaryTest2 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 0B000000000000000000000000000101; //5
+		int num2 = 0B11111111111111111111111111111011; //-5
+		
+		int sum = num1 + num2;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(sum);
+
+
+	}
+
+}
