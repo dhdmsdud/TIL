25a51518282b0ac45027b790a2ca29a0d975b0a0
ruby <dhdmsdud97@gmail.com>
2021-07-10 20:19:19 +0900

    JAVA

---
+++ JAVA/Chapter4/forEX2.java
@@ -0,0 +1,20 @@
+package loopEX;
+
+public class forEX2 {
+
+	public static void main(String[] args) {
+		
+		int dan;
+		int times;
+		
+		for(dan = 2; dan <= 9; dan++) {
+			
+			for(times = 1; times<9; times++) {
+				System.out.println(dan + "X" + times + "=" + dan*times);
+		}
+		System.out.println();
+		}
+	}
+}
+
+
---
+++ JAVA/Chapter4/forEX3.java
@@ -0,0 +1,13 @@
+package loopEX;
+
+public class forEX3 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i < 10; i++) {
+			System.out.println("hello, world");
+		}
+
+	}
+
+}
