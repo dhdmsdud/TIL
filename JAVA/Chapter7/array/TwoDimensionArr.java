de06ee401f5d5bbf6c07525d2b8a59b0453ba6d1
ruby <dhdmsdud97@gmail.com>
2021-07-22 20:19:19 +0900

    JAVA

---
+++ JAVA/Chapter7/array/TwoDimensionArr.java
@@ -0,0 +1,13 @@
+package array;
+
+public class TwoDimensionArr {
+
+	public static void main(String[] args) {
+		
+		int[][] arr = new int[2][3];
+		
+		System.out.println(arr.length);
+		System.out.println(arr[0].length);
+	}
+
+}
---
+++ JAVA/Chapter7/array/TwoDimensionArr2.java
@@ -0,0 +1,15 @@
+package array;
+
+public class TwoDimensionArr2 {
+
+	public static void main(String[] args) {
+		
+		int[][] arr = {{1,2,3}, {4,5,6}};
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				System.out.println(arr[i][j]);
+			}
+		}
+	}
+}
